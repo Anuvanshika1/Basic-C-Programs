a19310140c857b415309911672e369eb5d3e93c1
Anuvanshika Rana <122532406+Anuvanshika1@users.noreply.github.com>
2025-08-22 21:13:57 +0530

    Add files via upload

---
+++ Area_Perimeter_Rectangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+	int main()
+	{ 
+	    int L,W,A,P;
+	    printf("Give the value of L: ");
+	    scanf("%d",&L);
+	    printf("Give the value of W: ");
+	    scanf("%d",&W);
+	    A=L*W;   
+	    P=2*(L+W);  
+	    printf("Area of rectangle is: %d \n",A);
+	      
+	    printf("Perimeter of rectangle is :%d \n",P);
+
+return 0;
+}
+
---
+++ Area_Perimeter_circle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int main()
+	{ 
+	    float r,A,P;
+	    printf("Give the value of r: ");
+	    scanf("%f",&r);
+	    A=3.14*r*r;   
+	    P=2*3.14*r;  // Perimeter formula for circle 3.14 represents Pie value
+	    printf("Area of circle is: %f \n",A);
+	      
+	    printf("Perimeter of circle is :%f \n",P);
+
+return 0;
+}
+
