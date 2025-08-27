3a433f02169fc65e4e44146d3fd79be7aa6bb07e
Anuvanshika Rana <122532406+Anuvanshika1@users.noreply.github.com>
2025-08-27 09:28:38 +0530

    Converting_days_month,year.c

--- Converting_days_month,year.c
+++ Converting_days_month,year.c
@@ -6,9 +6,9 @@ int main() {
 	printf("Enter the number of days: ");
 	scanf("%d",&d);
 	y=d/360; // this will give us the number of years
-	Y=d%360; // this will us the number of months
-	m=Y/30; // this will give us number of months
-	D=Y%30; // this will give us remaining days
+	Y=d%360; // this will us the number of days left after getting number of years
+	m=Y/30; // this will give us number of months from the remaining number of days
+	D=Y%30; // this will give us remaining days left after calculating number of months
 	printf("Number of years:%d, number of months: %d,number of days remaining:%d",y,m,D);
 	return 0;
 
