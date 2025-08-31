a981bb9c2266e8bcd06aae7732c3f74f407522ae
Anuvanshika Rana <122532406+Anuvanshika1@users.noreply.github.com>
2025-08-31 18:49:24 +0530

    Day5.c

--- Day5.c
+++ Day5.c
@@ -16,6 +16,19 @@ int main(){
     return 0;
     
 }
+// Compound Interest
+#include <stdio.h>
+#include <math.h>
+int main() { 
+    float P,r,t,A,C;
+    printf("Enter the value of Principal value(P),rate(r) and time(t): ");
+    scanf("%f %f %f",&P,&r,&t);
+
+    A= P* pow((1 + r/100),t);
+    C=A-P;
+    printf("Compounded interest is: %.2f",C);
+    return 0;
+}
 
 // Write a program to convert user input seconds to Hours,minutes and second format
 #include <stdio.h>
