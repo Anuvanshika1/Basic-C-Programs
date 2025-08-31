5250af38c2ee85d6cea24cb7e00b8065b03e1c74
Anuvanshika Rana <122532406+Anuvanshika1@users.noreply.github.com>
2025-08-31 18:26:44 +0530

    Day9.c

--- Day9.c
+++ Day9.c
@@ -1,6 +1,30 @@
 // DAY 9
 // Write a program to find the roots of a quadratic equation and categorize them.
-
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float a,b,c,D,X1,X2;
+    printf("Enter the value of coffiecients of quadratic equation: ");
+    scanf("%f, %f, %f",&a,&b,&c);
+    D=b*b-4*a*c;
+  
+    if (D>0)
+    {
+         X1=(-b+sqrt(D))/(2*a);
+         X2=(-b-sqrt(D))/(2*a);
+        printf("Roots are real and different: %f,%f",X1,X2);
+    }
+    else if(D==0)
+    {
+        X1=X2=-b / (2*a);
+        printf("Roots are real and equal: %f,%f",X1,X2);
+    }
+    else
+    {
+        printf("Roots are complex");
+    }
+    return 0;
+}
 // Write a program to assign grades based on a percentage input.
 #include <stdio.h>
 int main(){
